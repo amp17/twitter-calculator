529d2cc3102cdf748d85b9b5ddee85716aaf9cfb
Atli Marcher <amp17@hi.is>
2017-12-10 21:26:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log('Bot starting');
+
+const Twit = require('twit');
+const math = require('mathjs');
+const config = require('./config.js');
+
+const T = new Twit(config);
+
+const stream = T.stream('user');
+stream.on('tweet', tweet);
+
+function tweet(msg) {
+  if (msg.in_reply_to_screen_name === 'twit_calc' && msg.user.screen_name !== 'twit_calc') {
+    console.log('Got message');
+    const txt = msg.text.replace(/@|[a-zA-z]+|\'|\./g, '');
+    const expr = txt.replace(/\s+/g, '');
+    const user = msg.user.screen_name;
+    const id = msg.id;
+    const patt = /(\d+(\.\d+)?[+-/*^]\d+(\.\d+)?)*/g;
+    if (expr && patt.test(expr)) reply(`@${user} ${expr} = ${math.eval(expr)}`, id);
+    else reply(`@${user} expression not valid`, id);
+  }
+}
+
+function reply(msg, id) {
+  const tweet = {
+    status: msg,
+    in_reply_to_status_id: id
+  }
+  T.post('statuses/update', tweet, (err, data, response) => {
+    if (!err) console.log(`Posted message ${msg}`);
+    else console.log(err);
+  });
+}
